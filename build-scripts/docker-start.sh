2a1d6d73d6ffc6504f621123c594357ee264dca6
Dudoleitor <dudoleitor@dudoleitor.com>
2021-09-27 00:27:39 +0200

    Initial commit

---
+++ build-scripts/docker-start.sh
@@ -0,0 +1,15 @@
+##################################################################
+# Script Name	  : docker-start.sh
+# Description	  : Used to start both nginx and stunnel
+# Args          : none
+# Author       	: dudoleitor
+# Email         : dudoleitor@dudoleitor.com
+# GitHub        : github.com/dudoleitor/docker-nginx-rtmps
+###################################################################
+
+#!/bin/sh
+/usr/bin/stunnel
+
+# nginx won't daemonize so that docker can detect
+# the process running inside the container
+/usr/local/nginx/sbin/nginx -g 'daemon off;'
